d6f2cda23e29184919087a69ae26997869c53808
fran.gimenez <fran.gimenez@trazable.io>
2019-11-12 16:20:47 +0100

    feat(instance): add simple vm_instance
    Created a simple google cloud vm_instance module

---
+++ modules/google/instances/basic-instance/main.tf
@@ -0,0 +1,26 @@
+variable "instance_name" {}
+variable "machine_type" {}
+variable "google_zone" {}
+variable "google_project_id" {}
+variable "machine_image" {}
+
+resource "google_compute_instance" "compute_instance" {
+  name                      = var.instance_name
+  machine_type              = var.machine_type
+  zone                      = var.google_zone
+
+  boot_disk {
+    initialize_params {
+      image = var.machine_image
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+  metadata = {
+    google-logging-enabled = true
+  }
+}
