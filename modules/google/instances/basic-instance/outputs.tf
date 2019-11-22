de48c8dd1cbfdfbcba1f3c7bd092f15646fbbbe6
fran.gimenez <fran.gimenez@trazable.io>
2019-11-22 15:53:08 +0100

    fix: was typed wrong sentence

--- modules/google/instances/basic-instance/outputs.tf
+++ modules/google/instances/basic-instance/outputs.tf
@@ -1,4 +1,4 @@
 # EXTERNAL MACHINE ADDRESS OF ONE MACHINE
 output "vm_machine_address" {
-  value = google_compute_instance.compute_instance.network_interface.0.acces_config.0.nat_ip
+  value = google_compute_instance.compute_instance.network_interface.0.access_config.0.nat_ip
 }
