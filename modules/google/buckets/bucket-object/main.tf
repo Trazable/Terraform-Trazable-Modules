d4a064c5cd0decc4a8b860675ed80c8031c09623
fran.gimenez <fran.gimenez@trazable.io>
2019-11-27 12:10:47 +0100

    fix(variable): change variable type to list

--- modules/google/buckets/bucket-object/main.tf
+++ modules/google/buckets/bucket-object/main.tf
@@ -1,6 +1,6 @@
 variable "google_project_id" {}
-variable "google_bucket_object_name" {}
-variable "google_bucket_name" { type = list }
+variable "google_bucket_object_name" { type = list }
+variable "google_bucket_name" {}
 
 resource "google_storage_bucket_object" "bucket_object" {
   count      = length(var.google_bucket_object_name)
