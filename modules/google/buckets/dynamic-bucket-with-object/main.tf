9b46eb059ba78c2eec680c536b7fd4fdd1f73d0c
fran.gimenez <fran.gimenez@trazable.io>
2020-02-03 13:50:15 +0100

    feat(bucket-with-object): allow dynamic cration
    Create multiple bucket_objects with their bucket parent, this module allow the directly dependency.

---
+++ modules/google/buckets/dynamic-bucket-with-object/main.tf
@@ -0,0 +1,17 @@
+variable "google_project_id" {}
+variable "google_region" {}
+variable "bucket_name" {}
+variable "bucket_object_name" { type = list }
+variable "object_source" {}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "${var.google_project_id}-${var.bucket_name}"
+  location = var.google_region
+}
+
+resource "google_storage_bucket_object" "bucket_object" {
+  count      = length(var.google.bucket_object_name)
+  name       = "${var.google_project_id}-${var.bucket_object_name[count.index]}"
+  bucket     = google_storage_bucket.bucket.name
+  source     = var.object_source
+}
