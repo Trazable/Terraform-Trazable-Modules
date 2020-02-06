debced99542ada92e3493d06536aa52f233bbd38
fran.gimenez <fran.gimenez@trazable.io>
2020-02-06 13:08:03 +0100

    feat(dependency): allow dependency between modules
    With a jerarchy trick of dependencies and variable dependencies is possible create dependencies between modules, explanation of the behavior in the code.

--- modules/google/instances/basic-instance/main.tf
+++ modules/google/instances/basic-instance/main.tf
@@ -1,11 +1,3 @@
-variable "instance_name" {}
-variable "machine_type" {}
-variable "google_zone" {}
-variable "google_project_id" {}
-variable "machine_image" {}
-variable "user_data_file" {}
-variable "service_accounts_scopes" { type = list }
-
 resource "google_compute_instance" "compute_instance" {
   name                      = var.instance_name
   machine_type              = var.machine_type
@@ -24,10 +16,35 @@ resource "google_compute_instance" "compute_instance" {
 
   metadata = {
     google-logging-enabled = true
-    user-data = file(var.user_data_file)
+    user-data              = file("${path.module}/${var.user_data_file}")
   }
 
-  service_account {
-    scopes = var.service_accounts_scopes
+  depends_on = [null_resource.dependency_getter]
+}
+
+# TERRAFORM DEPENDENCIES
+#
+# Module A : JERARCHY = VAR DEPENDENCIES > DEPENDENCY_GETTER > REAL RESOURCE > DEPENDENCY SETTER > OUTPUT DEPENDED_ON (ID)
+# 1 - Dependency created with the value of the variable dependencies from external module (MODULE B) (null_resource.dependecy_getter => var.dependencies)
+# 2 - Real resource (google_compute_instance.compute_instance) has a dependency of null_resource_dependency_getter
+# 3 - Output depended_on implicit dependency of null_resource.dependency_setter.id 
+# To create a dependency add in your module: dependencies = [module.MODULE_B_name.depended_on]
+# Solution created by medium.com/@aniket10051994
+resource "null_resource" "dependency_getter" {
+  provisioner "local-exec" {
+    command = "echo ${length(var.dependencies)}"
   }
+}
+
+resource "null_resource" "dependency_setter" {
+  depends_on = [google_compute_instance.compute_instance]
+}
+
+output "depended_on" {
+  value = null_resource.dependency_setter.id
+}
+
+variable "dependencies" {
+  type    = list
+  default = []
 }
--- modules/google/kms/kms-ring/main.tf
+++ modules/google/kms/kms-ring/main.tf
@@ -7,6 +7,33 @@ resource "google_kms_key_ring" "kms_ring" {
   project  = var.google_project_id
   name     = var.kms_ring_name[count.index]
   location = var.google_region
+
+  depends_on = [null_resource.dependency_getter]
 }
 
+# TERRAFORM DEPENDENCIES
+#
+# Module A : JERARCHY = VAR DEPENDENCIES > DEPENDENCY_GETTER > REAL RESOURCE > DEPENDENCY SETTER > OUTPUT DEPENDED_ON (ID)
+# 1 - Dependency created with the value of the variable dependencies from external module (MODULE B) (null_resource.dependecy_getter => var.dependencies)
+# 2 - Real resource (google_kms_key_ring.key_ring) has a dependency of null_resource_dependency_getter
+# 3 - Output depended_on implicit dependency of null_resource.dependency_setter.id 
+# To create a dependency add in your module: dependencies = [module.MODULE_B_name.depended_on]
+# Solution created by medium.com/@aniket10051994
+resource "null_resource" "dependency_getter" {
+  provisioner "local-exec" {
+    command = "echo ${length(var.dependencies)}"
+  }
+}
 
+resource "null_resource" "dependency_setter" {
+  depends_on = [google_kms_key_ring.kms_ring]
+}
+
+output "depended_on" {
+  value = null_resource.dependency_setter.id
+}
+
+variable "dependencies" {
+  type    = list
+  default = []
+}
